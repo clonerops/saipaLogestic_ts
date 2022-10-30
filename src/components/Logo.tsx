9c788929efe80fdfa009a185bffc7ab1e65b1d1d
clonerops <114849678+clonerops@users.noreply.github.com>
2022-10-30 11:06:48 +0330

    'initial'

---
+++ src/components/Logo.tsx
@@ -0,0 +1,11 @@
+import SaipaLogo from '../assets/images/saipa-image.png'
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            <img src={SaipaLogo} className='logo__img' />
+        </div>
+    )
+}
+
+export default Logo
